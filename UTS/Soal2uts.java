cdef6b521d16250c9e69201d56f571e9eaf88724
Sallma Diana Putri <80019527+Sallmadianaputri@users.noreply.github.com>
2021-04-22 08:43:11 +0700

    Add files via upload

---
+++ UTS/Soal2uts.java
@@ -0,0 +1,17 @@
+/*
+ * Sallma Diana Putri 11200930000012
+ */
+package UTS;
+
+public class Soal2uts {
+        String Name;
+        int Age;
+    void cetakName(String Name) {
+        System.out.println("Nama     :  " + Name);
+        
+    }
+    void cetakAge(int Age){
+    System.out.println("Age    :  " + Age);
+    }
+    
+}
---
+++ UTS/Soal2uts1.java
@@ -0,0 +1,18 @@
+/*
+ * sallma diana putri 11200930000012
+ */
+package UTS;
+
+
+public class Soal2uts1 {
+    public static void main(String[] args){
+        
+        Soal2uts Soal2uts1 = new Soal2uts();
+        //person1
+        Soal2uts1.cetakName("Joe Smith");
+        Soal2uts1.cetakAge(24);
+        //person2
+        Soal2uts1.cetakName("Mary Sharp");
+        Soal2uts1.cetakAge(52);
+    }
+}
